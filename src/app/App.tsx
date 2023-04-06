66ba5f7c1073ebf39efeb661e53baa21eae46f27
Anna Slivankova <annslivankova@gmail.com>
2023-04-06 18:57:03 +0300

    fix: refactor after merge into master

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import './App.css'
+import { CircularProgress } from '@mui/material'
+import { useSelector } from 'react-redux'
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { Error } from '../Component/404/Error'
@@ -8,12 +10,10 @@ import { initialized } from '../Component/auth/auth-slice'
 import { Signin } from '../Component/auth/signin/Signin'
 import { Signup } from '../Component/auth/signup/Signup'
 import { ErrorSnackbar } from '../Component/ErrorSnackbar/ErrorSnackBar'
-import { NewPass } from '../Component/newPass/newPass'
-import { PassRecovery } from '../Component/passwordRecovery/passRecovery'
 import { CreateNewPassword } from '../Component/forgot_password/components/CreateNewPassword'
 import { ForgotPassword } from '../Component/forgot_password/components/ForgotPassword'
 import { CheckEmail } from '../Component/forgot_password/components/СheckEmail'
-import { Login } from '../Component/Login/Login'
+import { PassRecovery } from '../Component/passwordRecovery/passRecovery'
 import { Profile } from '../Component/Profile/Profile'
 import { Registration } from '../Component/Registration/Registration'
 import { Test } from '../Component/test/test'
@@ -48,7 +48,6 @@ export function App() {
       <ErrorSnackbar />
       <Routes>
         <Route path={'/'} element={<Profile />} />
-        <Route path={'/login'} element={<Login />} />
         <Route path={'/registration'} element={<Registration />} />
         <Route path={'/profile'} element={<Profile />} />
         <Route path={'/set-new-password/:id'} element={<CreateNewPassword />} />
@@ -57,7 +56,6 @@ export function App() {
         <Route path={'/signin'} element={<Signin />} />
         <Route path={'/signup'} element={<Signup />} />
         <Route path={'/passrecovery'} element={<PassRecovery />} />
-        <Route path={'/newpass'} element={<NewPass />} />
         <Route path={'/test'} element={<Test />} />
         <Route path={'/404'} element={<Error />} />
         <Route path={'*'} element={<Navigate to={'/404'} />} />
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { Provider } from 'react-redux'
-import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import { App } from './app/App'
 import { store } from './app/store'
@@ -12,11 +12,11 @@ import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>
 )
 
 // If you want to start measuring performance in your app, pass a function
